b63cdadce3cabda011047631d35b612dd4d69084
svik75 <56226167+svik75@users.noreply.github.com>
2020-03-04 21:57:17 +0300

    Initial commit from gatsby: (https://github.com/anubhavsrivastava/gatsby-starter-aerial.git)

---
+++ config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  siteTitle: 'Gatsby Starter Aerial', // <title>
+  manifestName: 'Aerial',
+  manifestShortName: 'Portfolio', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-aerial/`, // This path is subpath of your hosting https://domain/portfolio
+  // social
+
+  authorName: 'Anubhav Srivastava',
+  authorHeading: 'Web Developer - JAMStack - Open Source enthusiast',
+  authorSocialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/anubhavsrivastava',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/onlyanubhav',
+    },
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: 'https://facebook.com/theanubhav',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:anubhav.srivastava00@gmail.com',
+    },
+  ],
+};
